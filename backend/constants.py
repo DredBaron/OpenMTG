d0e602ad4f7eba794d28f0973740d2b75f0214ee
Todd Hahn <toddhahn73@proton.me>
2026-05-09 19:03:38 -0400

    Updated to use rate-limited API requester

--- backend/constants.py
+++ backend/constants.py
@@ -15,3 +15,10 @@ VERSIONS = {
     "API_VERSION": "1.6.0",
     "APPLICATION_VERSION": "1.6.0",
 }
+
+API_RATES = {
+    "RATE_LIMIT_RPS": "2",
+    "RATE_LIMIT_GAP": "0.5",
+    "ERROR_429_BACKOFF": "60",
+    "CALLER_TIMEOUT": "30",
+}
