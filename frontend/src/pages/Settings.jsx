4f779f63f171cd16c9b231a3d47de1b57c01a8f0
Todd Hahn <toddhahn73@proton.me>
2026-05-10 01:14:37 -0400

    Suppressed

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -224,6 +224,7 @@ export default function Settings() {
   });
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     if (pollFast && status?.stale_cards === 0) setPollFast(false);
   }, [status?.stale_cards, pollFast]);
 
@@ -233,6 +234,7 @@ export default function Settings() {
 
   useEffect(() => {
     if (currentSettings) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
       setForm({ price_refresh_hours: parseInt(currentSettings.price_refresh_hours) });
     }
   }, [currentSettings]);
