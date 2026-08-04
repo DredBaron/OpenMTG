999b1365c25c8cb1d6e285c530df7edd19845df7
Todd Hahn <toddhahn73@proton.me>
2026-08-03 21:08:43 -0400

    Added uploads and trades directory creation and trades migrations on startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
 cd /app
+mkdir -p /data/uploads/card_photos
+mkdir -p /data/trades
 alembic upgrade head
+alembic -c alembic_trades.ini upgrade head
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
